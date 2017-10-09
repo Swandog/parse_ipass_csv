2836d4bf0fcddcf0bcb2066e37dc008e438602a7
Kenneth Swanson <swandog@gmail.com>
2017-10-08 23:02:27 -0500

    Initial version of python replacement

---
+++ bin/parse.py
@@ -0,0 +1,25 @@
+#/usr/bin/env python3
+
+import csv
+import sys
+
+with open(sys.argv[1]) as csvfile:
+    reader=csv.reader(csvfile)
+
+    headers_line=next(reader)
+    headers=dict()
+    for i in range(len(headers_line)):
+        headers[headers_line[i]]=i
+
+    print('Date,Payee,Category,Memo,Outflow,Inflow')
+    for row in reader:
+        date,_,_=row[headers['Transaction Date']].split()
+
+        payee='-'.join(row[headers['Location']].split('-')[0:2])
+        category=''
+        memo=row[headers['Location']]
+        outflow=abs(float(row[headers['Amount']].replace('$', '')))
+        inflow=''
+
+        print(date, payee, category, memo, outflow, inflow, sep=',')
+
